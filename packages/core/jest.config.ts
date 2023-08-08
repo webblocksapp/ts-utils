1fa2485ae55a8d939db764d41d00aba20797f215
Mauricio Rivera <mauriver21@gmail.com>
2023-08-07 21:12:08 -0500

    Jest setup + add formatObjectPath fn

---
+++ packages/core/jest.config.ts
@@ -0,0 +1,22 @@
+import { Config } from '@jest/types';
+import { pathsToModuleNameMapper } from 'ts-jest';
+import { compilerOptions } from './tsconfig.json';
+
+const config: Config.InitialOptions = {
+  verbose: true,
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>',
+  }),
+  globals: {
+    'ts-jest': {
+      isolatedModules: true,
+    },
+  },
+  transform: {
+    '\\.[jt]sx?$': 'babel-jest',
+  },
+  testMatch: ['**/?(*.)(test).ts?(x)'],
+  testEnvironment: 'jsdom',
+};
+
+export default config;
